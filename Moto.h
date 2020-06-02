9d644547fe2e4f42a28607b03372f885c51ab7cf
bm7 <bayangp0@gmail.com>
2020-06-02 23:57:21 +0100

    Adding class

---
+++ Moto.h
@@ -0,0 +1,14 @@
+#ifndef MOTO_H_INCLUDED
+#define MOTO_H_INCLUDED
+#include "Vehicule.h"
+
+class Moto : public Vehicule
+{
+    public:
+    void show() const;
+
+    protected:
+    double m_vitesse; // velocity of the motorcycle, in french
+};
+
+#endif // MOTO_H_INCLUDED
---
+++ Vehicule.h
@@ -0,0 +1,12 @@
+#ifndef VEHICULE_H_INCLUDED
+#define VEHICULE_H_INCLUDED
+
+class Vehicule
+{
+    public:
+    void show() const;
+
+    protected:
+    int m_price
+};
+#endif // VEHICULE_H_INCLUDED
